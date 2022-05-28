e5923592ae0eca980a6b8dfeac07f0a154509927
iabtw <twitchisadeck@gmail.com>
2022-05-28 03:51:39 +0200

    feat(Windows): Audio controller detection
    * Files didn't stage for commit? Idk.

--- pysi/core/managers/audio.py
+++ pysi/core/managers/audio.py
@@ -6,7 +6,7 @@ class AudioManager(BaseManager[AudioController]):
     def __init__(self):
         pass
     
-    def audio_info(self) -> list[AudioController] | None:
+    def get_info(self) -> list[AudioController] | None:
         """
         Extracts information about the Audio controller(s)
         inside of the current system.
@@ -32,7 +32,45 @@ class AudioManager(BaseManager[AudioController]):
         raise NotImplementedError
 
     def _win(self) -> list[AudioController] | None:
-        raise NotImplementedError
+        try:
+            from wmi import WMI
+            from util.util import Util
+
+            HDAS = WMI().instances("Win32_SoundDevice")
+            CONTROLLERS = []
+
+            for HDA in HDAS:
+                pnpid = HDA.wmi_property("PNPDeviceID")
+
+                if (
+                    not pnpid or
+                    not getattr(pnpid, "value", False) or
+                    not "HDAUDIO" in pnpid.value
+                ):
+                    continue
+                else:
+                    pnpid = pnpid.value
+
+                name = HDA.wmi_property("ProductName").value
+                ven, dev = [x[1] for x in Util.extract_from_pnp(pnpid)]
+                codec = None
+
+                if "10ec" in ven.lower():
+                    name = f"Realtek ALC{dev.upper()}"
+                    codec = f"ALC{dev.upper()[2:].lstrip('0')}"
+
+                CONTROLLERS.append(
+                    AudioController(
+                        name,
+                        dev,
+                        ven,
+                        codec
+                    )
+                )
+
+            return CONTROLLERS
+        except Exception:
+            return []
     
     def _linux(self) -> list[AudioController] | None:
         raise NotImplementedError
---
+++ pysi/tests/audio.py
@@ -0,0 +1,20 @@
+def exec():
+    from core.managers.audio import AudioManager
+
+    hdas = AudioManager().get_info()
+
+    print(
+        "\n" + ("=" * 25) + "\n" +
+        "|\t  AUDIO   \t|" +
+        "\n" + ("=" * 25)
+    )
+
+    for hda in hdas:
+        print(
+            "\n".join([
+                f"Model: {hda.model}",
+                f"Device ID: {hda.dev_id}",
+                f"Vendor ID: {hda.ven_id}",
+                f"Codec: {hda.codec}"
+            ]) + "\n"
+        )
