0748c7e41a9cd051aedff3c7d40ffb9401269c14
iabtw <twitchisadeck@gmail.com>
2022-05-28 03:51:14 +0200

    feat(Windows): Audio controller detection

--- pysi/core/hardware/audio.py
+++ pysi/core/hardware/audio.py
@@ -4,7 +4,6 @@ class AudioController:
         model: str,
         dev_id: str,
         ven_id: str,
-        pci: str | None = None,
         codec: str | None = None
     ):
         self.model = model
@@ -16,16 +15,5 @@ class AudioController:
         self.ven_id = ven_id
         """ The Vendor ID of this Audio controller. """
 
-        self.pci = pci
-        """ The raw PCI path of this Audio controller (if available.) """
-
         self.codec = codec
         """ The ALC codec of this Audio controller (if available.) """
-
-        if (
-            type(self.ven_id) == str and
-            "10ec" in self.ven_id.lower()
-        ):
-            alc_codec = "ALC" + hex(int(self.ven_id, 16))[2:]
-
-            self.codec = alc_codec
