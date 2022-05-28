c113fd88080bb14e549b82757913f3a3a1889209
kernelBPD <twitchisadeck@gmail.com>
2022-05-28 21:54:09 +0200

    feat(macOS): GPU device detection

--- pysi/core/managers/gpu.py
+++ pysi/core/managers/gpu.py
@@ -28,8 +28,123 @@ class GPUManager(BaseManager[GPU]):
     # The following are marked private
     # since they're meant for
     # internal usage only.
-    def _osx(self) -> list[GPU] | None:
-        raise NotImplementedError
+    def _osx(self, default=True) -> list[GPU] | None:
+        try:
+            import core.helper.ioreg as ioreg
+            import binascii
+
+            if default:
+                device = {
+                    "IOProviderClass": "IOPCIDevice",
+                    # Bit mask matching, ensuring that the 3rd byte is one of the display controller (0x03).
+                    "IOPCIClassMatch": "0x03000000&0xff000000",
+                }
+            else:
+                device = {"IONameMatched": "gpu*"}
+
+            interface = ioreg.ioiterator_to_list(
+                ioreg.IOServiceGetMatchingServices(
+                    ioreg.kIOMasterPortDefault,
+                    device,
+                    None
+                )[1]
+            )
+
+            GPUS = []
+
+            for i in interface:
+                device = ioreg.corefoundation_to_native(
+                    ioreg.IORegistryEntryCreateCFProperties(
+                        i, 
+                        None, 
+                        ioreg.kCFAllocatorDefault, 
+                        ioreg.kNilOptions
+                    )
+                )[1]
+
+                # I don't know why there needs to be
+                # a try clause here, but it does.
+                try:
+                    # For Apple's AGX (ARM64 iGFX)
+
+                    if (
+                        not default and
+
+                        # If both return true, that means
+                        # we aren't dealing with a GPU device.
+                        not "gpu" in device.get("IONameMatched", "").lower() and
+                        not "AGX" in device.get("CFBundleIdentifierKernel", "")
+                    ):
+                        continue
+                except Exception:
+                    continue
+
+                model = device.get("model")
+                ven = "0x" + (
+                    binascii.b2a_hex(
+                        bytes(
+                            reversed(device.get("vendor-id"))
+                        )
+                    ).decode()[4:]
+                )
+
+                if not model:
+                    continue
+
+                if default:
+                    model = bytes(model).decode()
+                    model = model[:len(model) - 1]
+
+                    dev = "0x" + (
+                        binascii.b2a_hex(
+                            bytes(reversed(device.get("device-id")))
+                        ).decode()[4:]
+                    )
+
+                    plane = device.get("acpi-path", "")
+
+                    pci_path = Util.construct_pci_path(i)
+                    acpi_path = Util.construct_acpi_path(plane)
+
+                    GPUS.append(
+                        GPU(
+                            model,
+                            dev,
+                            ven,
+                            pci_path,
+                            acpi_path,
+                        )
+                    )
+                
+                else:
+                    gpuconf = device.get("GPUConfigurationVariable", {})
+                    dev = ""
+
+                    cores = gpuconf.get("num_cores")
+                    ne_cores = gpuconf.get("num_gps")
+                    gen = gpuconf.get("gpu_gen")
+
+                    GPUS.append(
+                        GPU(
+                            model,
+                            dev,
+                            ven,
+                            None,
+                            None,
+                            None,
+                            None,
+                            cores,
+                            ne_cores,
+                            gen
+                        )
+                    )
+
+            if default:
+                GPUS += self._osx(False)
+
+            return GPUS
+        except Exception:
+            return []
 
     def _win(self) -> list[GPU] | None:
         try:
@@ -48,10 +163,10 @@ class GPUManager(BaseManager[GPU]):
                 GPUS.append(
                     GPU(
                         model,
+                        dev,
+                        ven,
                         pci_path,
                         acpi_path,
-                        dev,
-                        ven
                     )
                 )
             
