21a4cec9fb04ada5ff78254c6559f8ed8f60434a
iabtw <twitchisadeck@gmail.com>
2022-05-28 03:52:02 +0200

    feat: GPU, Input device, RAM, NIC, HDA and Storage device detection tests

--- pysi/tests/run_all.py
+++ pysi/tests/run_all.py
@@ -1,5 +1,11 @@
-from tests import x86cpu, baseboard
+from tests import x86cpu, baseboard, gpu, input, memory, network, audio, storage
 
 def exec():
     x86cpu.exec()
     baseboard.exec()
+    gpu.exec()
+    input.exec()
+    memory.exec()
+    network.exec()
+    audio.exec()
+    storage.exec()
